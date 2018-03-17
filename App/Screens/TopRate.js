2353a54dfd16c48c5069250cab1433cb914db400
Ali Alizada <akbar.alizadeh5@gmail.com>
2018-03-17 11:33:08 -0700

    router

--- App/Screens/TopRate.js
+++ App/Screens/TopRate.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Icon } from 'native-base'
 
 export default class TopRate extends Component {
     render (){
         return ( 
             <View style={styles.container}>
                 <Text style={styles.title}>
+                    <Icon name={"md-search"}/>
                    Hello Top Rate movie Scren
                 </Text>
             </View>
