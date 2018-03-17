1f209e4efe9a4064f29a38fe2353de5ae0888537
Ali Alizada <akbar.alizadeh5@gmail.com>
2018-03-17 12:41:20 -0700

    add search bar

--- App/Screens/Search.js
+++ App/Screens/Search.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Container, Content, Header, Item, Input, Icon, Button } from 'native-base';
+import axios from 'axios';
 
 export default class Search extends Component {
+    state= {
+        text: null,
+        movies: null,
+    }
+    _handelSearch = () => {
+        const {text} = this.state
+        axios({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/top_rated?api_key=936564fe71e66aa13d11090a2555b5cb&language=en-US&page=1",
+        })
+            .then(({ data }) => 
+                this.setState({movies: data.results }) 
+            )
+    }
+    }
     render (){
         return ( 
-            <View style={styles.container}>
-                <Text style={styles.title}>
-                    Search
-                </Text>
-            </View>
+            <Container>
+                <Header searchBar rounded>
+                    <Item>
+                        <Icon name="ios-search" />
+                        <Input placeholder="Search"  onChangeText={text => this.setState({ text })}/>
+                        <Icon name="ios-people" />
+                    </Item>
+                    <Button transparent onPress={()=> this._handelSearch()}>
+                        <Text>Search</Text>
+                    </Button>
+                </Header>
+                <Content>
+                    <Text>{this.state.text}</Text>
+                </Content>
+            </Container>
         )
     }
 }
