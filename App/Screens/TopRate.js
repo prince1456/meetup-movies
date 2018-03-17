e47a109221ab6e102ba72aacc1333ad1cde5a1b6
Ali Alizada <akbar.alizadeh5@gmail.com>
2018-03-17 12:08:22 -0700

    add api keys

--- App/Screens/TopRate.js
+++ App/Screens/TopRate.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { Icon } from 'native-base'
+import axios from 'axios'
 
 export default class TopRate extends Component {
+    state = {
+        movies: null
+    }
+    componentDidMount(){
+        axios({
+            method: "GET",
+            url: "https://api.themoviedb.org/3/movie/top_rated?api_key=936564fe71e66aa13d11090a2555b5cb&language=en-US&page=1",
+        })
+        .then(({ data }) => this.setState({movies: data})  )
+    }
     render (){
         return ( 
             <View style={styles.container}>
